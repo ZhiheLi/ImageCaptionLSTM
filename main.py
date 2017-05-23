6849a252b0dd2504b1cc488847eed31da6aab784
Zhihe Li <ZhiheLi@users.noreply.github.com>
2017-05-23 16:24:24 +0800

    Update main.py
    Make learning rate adjustable

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pp = Preprocess()
 cnnDim, chrNum, cap_max_len, train_sample, valid_sample = pp.preprocess()
 t = BuildModel(cnnDim, chrNum, cap_max_len, train_sample, valid_sample)
 
-t.model_gen(embeddingDim=1024, denseNode= 512, lstmNode=1000)
+t.model_gen(lr = 0.001, embeddingDim=1024, denseNode= 512, lstmNode=1000)
 t.model_fit(batch_size=256, epochs=20, earlystop=10)
 
 #t.model.load_weights('model_weights.h5')
