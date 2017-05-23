6e43775e2669cce51c739fe2ecb0406f316f1566
Zhihe Li <ZhiheLi@users.noreply.github.com>
2017-05-23 16:31:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from build_model import BuildModel
 
 pp = Preprocess()
 cnnDim, chrNum, cap_max_len, train_sample, valid_sample = pp.preprocess()
-t = BuildModel(cnnDim, chrNum, cap_max_len, train_sample, valid_sample)
+ic = BuildModel(cnnDim, chrNum, cap_max_len, train_sample, valid_sample)
 
-t.model_gen(lr = 0.001, embeddingDim=1024, denseNode= 512, lstmNode=1000)
-t.model_fit(batch_size=256, epochs=20, earlystop=10)
+ic.model_gen(lr = 0.001, embeddingDim=1024, denseNode= 512, lstmNode=1000)
+ic.model_fit(batch_size=256, epochs=20, earlystop=10)
 
-#t.model.load_weights('model_weights.h5')
-t.cap_gen(beam_size=20, index=9000)
+#ic.model.load_weights('model_weights.h5')
+ic.cap_gen(beam_size=20, index=9000)
+
