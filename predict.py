ef23be45885315be2833884878ad004dd8faf70d
jianhua <lay.79523@gmail.com>
2017-06-10 01:39:19 +0800

    Modify the 'predict.py' script to support prediction for validation set

--- predict.py
+++ predict.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 #coding: utf-8
 
+"""Generate predicted captions for test set / validation set."""
+
 from build_model import BuildModel
+from sys import argv
 import pickle as pkl
 
-[cnnDim, chrNum, cap_max_len, train_sample, valid_sample] = pkl.load(open('preprocess.pkl', 'rb'))
-ic = BuildModel(cnnDim, chrNum, cap_max_len, train_sample, valid_sample)
-ic.model_gen(lr = 0.01, dropout=0.25, embeddingDim=512, regularCoeff=0.001)
+def main(dataset, prev_index):
+    assert(dataset in ('test', 'validation'))
+    [cnnDim, chrNum, cap_max_len, train_sample, valid_sample] = pkl.load(open('preprocess.pkl', 'rb'))
+    ic = BuildModel(cnnDim, chrNum, cap_max_len, train_sample, valid_sample)
+    ic.model_gen(lr=0.01, dropout=0.25, embeddingDim=512, regularCoeff=0.001)
 
-ic.cap_gen(beam_size=20, index=9000)
+    ic.cap_gen(beam_size=20, index=prev_index, dataset=dataset)
+
+
+if __name__ == '__main__':
+    # Usage: argv[0] [test/validation]
+    dataset = 'test'
+    prev_index = 9001
+    if len(argv) >= 2:
+        dataset = argv[1]
+        prev_index = 8001
+
+    main(dataset, prev_index)
